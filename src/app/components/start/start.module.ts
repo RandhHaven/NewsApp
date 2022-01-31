ffb3c2c74718f9722249f1de55c4f44c41a4c775
Randhyll <randhallr@portobellosouth.com>
2022-01-31 01:52:48 -0300

    update newapp

--- src/app/components/start/start.module.ts
+++ src/app/components/start/start.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { StartRoutingModule } from './start-routing.module';
 import { StartComponent } from './start.component';
 
